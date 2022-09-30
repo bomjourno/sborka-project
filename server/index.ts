25294c6107f5bbec63c2884d6a62867c62195f7b
Matvey Kham <mvham@mail.ru>
2022-10-01 00:05:53 +0800

    feat: add server

---
+++ server/index.ts
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = 8080;
+
+app.get('/api/shoes', (req, res) => {
+  res.send(require('./data.json'));
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
